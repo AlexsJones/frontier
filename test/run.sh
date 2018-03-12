0bd97b180920df8dc98fc648ceffcf7753692625
AlexsJones <tibbar@freedommail.ch>
2018-03-12 19:13:30 +0000

    updated with graph

--- test/run.sh
+++ test/run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-vegeta attack -rate=100 -duration=10s -targets=targets.txt > results.bin
+vegeta attack -rate=10000 -duration=10s -targets=targets.txt > results.bin
 vegeta report -inputs=results.bin -reporter=json > metrics.json
 cat results.bin | vegeta report -reporter=plot > plot.html
-cat results.bin | vegeta report -reporter="hist[0,100ms,200ms,300ms]"
-xdg-pen plot.html
+cat results.bin | vegeta report
+xdg-open plot.html
