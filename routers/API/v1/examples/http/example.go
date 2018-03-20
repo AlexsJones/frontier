75b841855fb2ca7dfda35e13e0d1768e2595885f
AlexsJones <tibbar@freedommail.ch>
2018-03-20 08:59:07 +0000

    updated

--- routers/API/v1/examples/http/example.go
+++ routers/API/v1/examples/http/example.go
@@ -2,7 +2,7 @@ package http
 
 import "net/http"
 
-//Post is for illustrative purposes of how to use the processing API
+//Post is for illustrative purposes of how to use the API
 func Post(arg1 http.ResponseWriter, arg2 *http.Request) {
 
 	arg1.WriteHeader(http.StatusOK)
