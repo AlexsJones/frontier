106994d04acae438923eb06f61aca53d9bfa8666
AlexsJones <tibbar@freedommail.ch>
2018-05-29 20:33:40 +0100

    updated with simple s3 fetch example

--- test/s3_test/post.sh
+++ test/s3_test/post.sh
@@ -1,2 +1,2 @@
 #fetch some data from an s3 bucket
-curl -X POST -H "X-HEADER-REGION: eu-west-1" -H "X-HEADER-BUCKET: testing.beamery.com" http://localhost:8080/api/v1/bucketfetch -d '{}'
+curl -X POST -H "X-HEADER-REGION: SOMEREGION" -H "X-HEADER-BUCKET: COMPANY.com" http://localhost:8080/api/v1/bucketfetch -d '{}'
